c7fc3c5705e16a74e72d5479ea6fc10ff6eecabb
RomanPR <prikhodjko.roman@gmail.com>
2025-05-11 20:28:12 +0300

    input encoding fix

--- NET_proj/Program.cs
+++ NET_proj/Program.cs
@@ -11,7 +11,6 @@ class Program
 
     static void Main() {
         Console.OutputEncoding = System.Text.Encoding.UTF8;
-        Console.InputEncoding = System.Text.Encoding.UTF8;
         CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
 
         var exit = false;
--- NET_proj/obj/Debug/net8.0/NET_proj.AssemblyInfo.cs
+++ NET_proj/obj/Debug/net8.0/NET_proj.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NET_proj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+984240353271aef280b3ef09e84a9284c07442dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56b06016aea866e19428791fdb614e80af875c0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("NET_proj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NET_proj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
